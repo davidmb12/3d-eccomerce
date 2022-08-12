488c8d8924e0aa05a9f8680de15ae759d409915f
damu-odoo <damu@odoo.com>
2022-08-12 12:20:35 -0500

    [ADD] Added model page and three js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
+import '../styles/modelPage.css'
+
 import type { AppProps } from 'next/app'
+import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Navbar></Navbar>
+      <Component {...pageProps} />
+    </>
+    
+  ) 
+  
 }
 
 export default MyApp
