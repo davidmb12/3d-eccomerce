dcd32a60fe79077648a736ba80acfacc250874f5
davidmb12 <david.murillo@cetys.edu.mx>
2022-08-15 22:29:26 -0500

    Added modules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from 'next/app'
 import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  
   return(
     <>
       <Navbar></Navbar>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-
+import Hero from '../components/Hero'
 const Home: NextPage = () => {
+
+  
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -11,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        
+        <Hero></Hero>
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
